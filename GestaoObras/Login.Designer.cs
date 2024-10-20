ccdcdb1be6e49d66f029cf33a7c67eebfa748747
Mafalda <84814390+Sydn3y171@users.noreply.github.com>
2024-10-20 11:27:02 +0100

    Added a login page
    New login page and connection to BD

---
+++ GestaoObras/Login.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace GestaoObras
+{
+    partial class Login
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            label_Username = new Label();
+            label_Password = new Label();
+            textBox_Username = new TextBox();
+            textBox_Password = new TextBox();
+            button_Login = new Button();
+            button_Clear = new Button();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Century Gothic", 13.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.Location = new Point(161, 97);
+            label1.Name = "label1";
+            label1.Size = new Size(281, 27);
+            label1.TabIndex = 0;
+            label1.Text = "Welcome to Constructly";
+            label1.Click += label1_Click;
+            // 
+            // label_Username
+            // 
+            label_Username.AutoSize = true;
+            label_Username.Font = new Font("Century Gothic", 10.8F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label_Username.Location = new Point(73, 199);
+            label_Username.Name = "label_Username";
+            label_Username.Size = new Size(108, 21);
+            label_Username.TabIndex = 1;
+            label_Username.Text = "Username :";
+            // 
+            // label_Password
+            // 
+            label_Password.AutoSize = true;
+            label_Password.Font = new Font("Century Gothic", 10.8F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label_Password.Location = new Point(73, 280);
+            label_Password.Name = "label_Password";
+            label_Password.Size = new Size(100, 21);
+            label_Password.TabIndex = 2;
+            label_Password.Text = "Password :";
+            // 
+            // textBox_Username
+            // 
+            textBox_Username.Location = new Point(210, 197);
+            textBox_Username.Name = "textBox_Username";
+            textBox_Username.Size = new Size(232, 27);
+            textBox_Username.TabIndex = 3;
+            // 
+            // textBox_Password
+            // 
+            textBox_Password.Location = new Point(210, 274);
+            textBox_Password.Name = "textBox_Password";
+            textBox_Password.Size = new Size(232, 27);
+            textBox_Password.TabIndex = 4;
+            textBox_Password.TextChanged += textBox_Password_TextChanged;
+            // 
+            // button_Login
+            // 
+            button_Login.Location = new Point(348, 361);
+            button_Login.Name = "button_Login";
+            button_Login.Size = new Size(94, 29);
+            button_Login.TabIndex = 5;
+            button_Login.Text = "Login";
+            button_Login.UseVisualStyleBackColor = true;
+            button_Login.Click += button_Login_Click;
+            // 
+            // button_Clear
+            // 
+            button_Clear.Location = new Point(210, 361);
+            button_Clear.Name = "button_Clear";
+            button_Clear.Size = new Size(94, 29);
+            button_Clear.TabIndex = 6;
+            button_Clear.Text = "Clear";
+            button_Clear.UseVisualStyleBackColor = true;
+            button_Clear.Click += button_Clear_Click;
+            // 
+            // Login
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(597, 461);
+            Controls.Add(button_Clear);
+            Controls.Add(button_Login);
+            Controls.Add(textBox_Password);
+            Controls.Add(textBox_Username);
+            Controls.Add(label_Password);
+            Controls.Add(label_Username);
+            Controls.Add(label1);
+            Name = "Login";
+            Text = "Login";
+            Load += Login_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label_Username;
+        private Label label_Password;
+        private TextBox textBox_Username;
+        private TextBox textBox_Password;
+        private Button button_Login;
+        private Button button_Clear;
+    }
+}
---
+++ GestaoObras/Login.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace GestaoObras
+{
+    public partial class Login : Form
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        SqlConnection conn = new SqlConnection(@"Data Source=LAPTOP-ENCAPB3M\SQLEXPRESS;Initial Catalog=GestaoObrasConstrucaoCivil;Integrated Security=True;TrustServerCertificate=True;");
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Login_Load(object sender, EventArgs e)
+        {
+
+
+
+        }
+
+        private void button_Login_Click(object sender, EventArgs e)
+        {
+            string username, user_password;
+
+            username = textBox_Username.Text;
+            user_password = textBox_Password.Text;
+
+            try
+            {
+                string querry = "SELECT * FROM Login_users WHERE username = '" + textBox_Username.Text + "' AND password = '" + textBox_Password.Text + "'";
+                SqlDataAdapter sda = new SqlDataAdapter(querry, conn);
+
+                DataTable dtable = new DataTable();
+                sda.Fill(dtable);
+
+                if (dtable.Rows.Count > 0)
+                {
+                    username = textBox_Username.Text;
+                    user_password = textBox_Password.Text;
+
+                    //page that needed to be load next
+                    Menu menuForm = new Menu();
+                    menuForm.Show();
+                    this.Hide();
+                }
+                else
+                {
+                    MessageBox.Show("Invalid login credentials", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    textBox_Username.Clear();
+                    textBox_Password.Clear();
+
+                    textBox_Username.Focus();
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Error");
+            }
+            finally
+            {
+                conn.Close();
+            }
+        }
+
+        private void button_Clear_Click(object sender, EventArgs e)
+        {
+            textBox_Username.Clear();
+            textBox_Password.Clear();
+
+            textBox_Username.Focus();
+        }
+
+        private void textBox_Password_TextChanged(object sender, EventArgs e)
+        {
+            textBox_Password.PasswordChar = '*';
+        }
+    }
+}
